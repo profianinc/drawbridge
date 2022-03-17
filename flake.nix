3303a79efff97d210f799f49bfbffb8b626c8e4e
Roman Volosatovs <roman@profian.com>
2022-03-17 12:42:31 +0100

    build(nix): add doc tooling
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- flake.nix
+++ flake.nix
@@ -11,17 +11,39 @@
   outputs = { self, nixpkgs, flake-utils, fenix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        apiSpec = "api/api.yml";
+        docOutput = "doc/index.html";
 
-        rust = fenix.packages."${system}".fromToolchainFile {
-          file = ./rust-toolchain.toml;
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nix = "${pkgs.nix}/bin/nix --extra-experimental-features flakes --extra-experimental-features nix-command";
+
+        rust = fenix.packages."${system}".fromToolchainFile { file = ./rust-toolchain.toml; };
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = [
             rust
+
+            pkgs.redoc-cli
+
+            (pkgs.writeShellScriptBin "build-doc" ''
+              ${nix} build '.#doc' -o '${docOutput}'
+            '')
+            (pkgs.writeShellScriptBin "watch-doc" ''
+              ${pkgs.fd}/bin/fd | ${pkgs.ripgrep}/bin/rg 'api.yml' | ${pkgs.entr}/bin/entr -rs "${pkgs.redoc-cli}/bin/redoc-cli serve '${apiSpec}'"
+            '')
           ];
+        };
+
+        packages = flake-utils.lib.flattenTree {
+          doc = pkgs.stdenv.mkDerivation {
+            name = "doc";
+            src = self;
+            buildInputs = [ pkgs.redoc-cli ];
+            buildPhase = "redoc-cli bundle '${apiSpec}' -o index.html";
+            installPhase = "mv index.html $out";
+          };
         };
       }
     );
